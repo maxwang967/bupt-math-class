a64d3be5e8ba03b77476602461e38af40a3fea59
morningstarwang <morningstarwang@outlook.com>
2020-05-22 20:37:55 +0800

    Initial commit

---
+++ eng_value.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/22 8:32 下午
+# @Author  : morningstarwang
+# @FileName: eng_value.py
+# @Blog: wangchenxing.com
+
+
+def power_eng(a):
+    """
+    幂法求矩阵最大特征值
+    :param a: 待求矩阵
+    :return: 最大特征值，特征向量
+    """
+    eng_value = None
+    eng_vector = None
+    return eng_value, eng_vector
+
+
+
+if __name__ == '__main__':
+    pass
