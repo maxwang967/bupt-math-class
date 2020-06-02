fa638ac5b2b58c2819ab0712f0d04be40fa1228b
Jia Yupeng <jypjypjypjyp@hotmail.com>
2020-06-02 11:47:50 +0800

    question_2

--- lab_square_root/question_2/main.c
+++ lab_square_root/question_2/main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <time.h>
 
+double n_loop = 0;
+
 unsigned get_s(int start, int end, unsigned c)
 {
     int mid = (start + end) / 2;
@@ -11,7 +13,7 @@ unsigned get_s(int start, int end, unsigned c)
     {
         if ((a >> 2) < c)
             return mid;
-        return get_s(start, mid, c);
+        return get_s(start, mid - 1, c);
     }
     else
     {
@@ -19,7 +21,7 @@ unsigned get_s(int start, int end, unsigned c)
         {
             return end;
         }
-        return get_s(mid, start, c);
+        return get_s(mid + 1, end, c);
     }
 }
 
@@ -27,14 +29,14 @@ unsigned my_isqrt(unsigned c)
 {
     if (c <= 1)
         return c;
-    unsigned c1 = c - 1;
-    int s = get_s(0, 31, c);
+    int s = get_s(0, 16, c);
     int x0 = 1 << s;
     int x1 = (x0 + (c >> s)) >> 1;
     while (x1 < x0)
     {
         x0 = x1;
         x1 = (x0 + c / x0) >> 1;
+        n_loop++;
     }
     return x0;
 }
@@ -47,6 +49,7 @@ int isqrt2(unsigned x)
         b = 65535; //  a <= sqrt(x) <= b
     do
     {
+        n_loop++;
         int m = (a + b) >> 1;
         if (m * m > x)
             b = m - 1;
@@ -60,7 +63,7 @@ int isqrt3(unsigned x)
 {
     if (x <= 1)
         return x;
-    int x1 = x - 1;
+    unsigned int x1 = x - 1;
     int s = 1;
     if (x1 > 65535)
     {
@@ -81,10 +84,11 @@ int isqrt3(unsigned x)
     {
         s += 1;
     }
-    int x0 = 1 << s;
+    unsigned int x0 = 1 << s;
     x1 = (x0 + (x >> s)) >> 1;
     while (x1 < x0)
     {
+        n_loop++;
         x0 = x1;
         x1 = (x0 + x / x0) >> 1;
     }
@@ -107,8 +111,9 @@ unsigned int isqrt4(unsigned long M)
     }
     for (i = 15; i > 0; i--)
     {
+        n_loop++;
         N <<= 1;
-        tmp <<= 2;
+        tmp <<= 2;
         tmp += (M >> 30);
         ttp = N;
         ttp = (ttp << 1) + 1;
@@ -122,7 +127,7 @@ unsigned int isqrt4(unsigned long M)
     return N;
 }
 
-int main()
+void test_unsigned()
 {
     clock_t start, end;
     double err = 0;
@@ -130,22 +135,495 @@ int main()
     double rel_err = 0;
     double max_rel_err = 0;
     start = clock();
-    for (int i = 0; i <= __INT_MAX__; i++)
+    for (unsigned i = 1; i != 0; i++)
     {
-        double r = my_isqrt(i);
-        double e = fabs(i - );
-        double r_e = e / i;
+        unsigned r = sqrt(i);
+        // if (i % (__UINT32_MAX__ / 100) == 0)
+        // {
+        //     printf("%d\n", i / (__UINT32_MAX__ / 100));
+        // }
+    }
+    end = clock();
+    double time1 = (double)(end - start) * 1000 / CLOCKS_PER_SEC;
+    printf("time1=%.20f\n", time1);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = 1; i != 0; i++)
+    {
+        unsigned r = my_isqrt(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+        if (i % (__UINT32_MAX__ / 10) == 0)
+        {
+            printf("%d\n", i / (__UINT32_MAX__ / 10));
+        }
+    }
+    err = err / __UINT32_MAX__;
+    rel_err = rel_err / __UINT32_MAX__;
+    end = clock();
+    printf("time2=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err2=%.20f\n", err);
+    printf("rel_err2=%.20f\n", rel_err);
+    printf("max_err2=%.20f\n", max_err);
+    printf("max_rel_err2=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / __UINT32_MAX__);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = 1; i != 0; i++)
+    {
+        unsigned r = isqrt2(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+        if (i % (__UINT32_MAX__ / 10) == 0)
+        {
+            printf("%d\n", i / (__UINT32_MAX__ / 10));
+        }
+    }
+    err = err / __UINT32_MAX__;
+    rel_err = rel_err / __UINT32_MAX__;
+    end = clock();
+    printf("time3=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err3=%.20f\n", err);
+    printf("rel_err3=%.20f\n", rel_err);
+    printf("max_err3=%.20f\n", max_err);
+    printf("max_rel_err3=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / __UINT32_MAX__);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = 1; i != 0; i++)
+    {
+        unsigned r = isqrt3(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+        if (i % (__UINT32_MAX__ / 10) == 0)
+        {
+            printf("%d\n", i / (__UINT32_MAX__ / 10));
+        }
+    }
+    err = err / __UINT32_MAX__;
+    rel_err = rel_err / __UINT32_MAX__;
+    end = clock();
+    printf("time4=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err4=%.20f\n", err);
+    printf("rel_err4=%.20f\n", rel_err);
+    printf("max_err4=%.20f\n", max_err);
+    printf("max_rel_err4=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / __UINT32_MAX__);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = 1; i != 0; i++)
+    {
+        unsigned r = isqrt4(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+        if (i % (__UINT32_MAX__ / 10) == 0)
+        {
+            printf("%d\n", i / (__UINT32_MAX__ / 10));
+        }
+    }
+    err = err / __UINT32_MAX__;
+    rel_err = rel_err / __UINT32_MAX__;
+    end = clock();
+    printf("time5=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err5=%.20f\n", err);
+    printf("rel_err5=%.20f\n", rel_err);
+    printf("max_err5=%.20f\n", max_err);
+    printf("max_rel_err5=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / __UINT32_MAX__);
+}
+
+unsigned my_isqrt_op(unsigned c)
+{
+    if (c <= 1)
+        return c;
+    unsigned a = 1;
+    unsigned s = 0;
+    // 根据权重得出的二叉搜索树
+    if (c < a << 4)
+    {
+        if (c < a << 2)
+        {
+            if (c < a << 1)
+                s = 1;
+            else
+                s = 2;
+        }
+        else
+        {
+            if (c < a << 3)
+                s = 3;
+            else
+                s = 4;
+        }
+    }
+    else
+    {
+        if (c < a << 6)
+        {
+            if (c < a << 5)
+                s = 5;
+            else
+                s = 6;
+        }
+        else
+        {
+            if (c < a << 7)
+                s = 7;
+            else
+                s = 8;
+            if (c >= a << 8)
+            {
+                if (c < a << 12)
+                {
+                    if (c < a << 10)
+                    {
+                        if (c < a << 9)
+                            s = 9;
+                        else
+                            s = 10;
+                    }
+                    else
+                    {
+                        if (c < a << 11)
+                            s = 11;
+                        else
+                            s = 12;
+                    }
+                }
+                else
+                {
+                    if (c < a << 14)
+                    {
+                        if (c < a << 13)
+                            s = 13;
+                        else
+                            s = 14;
+                    }
+                    else
+                    {
+                        if (c < a << 15)
+                            s = 15;
+                        else
+                            s = 16;
+                    }
+                }
+            }
+        }
+    }
+    int x0 = 1 << s;
+    int x1 = (x0 + (c >> s)) >> 1;
+    while (x1 < x0)
+    {
+        x0 = x1;
+        x1 = (x0 + c / x0) >> 1;
+        n_loop++;
+    }
+    return x0;
+}
+
+void test_op_l()
+{
+    clock_t start, end;
+    double err = 0;
+    double max_err = 0;
+    double rel_err = 0;
+    double max_rel_err = 0;
+    const unsigned n = ((1 << 8) + 1) * ((1 << 8) + 1) - 1;
+    start = clock();
+    for (unsigned i = 1; i <= n; i++)
+    {
+        unsigned r = sqrt(i);
+        // if (i % (__UINT32_MAX__ / 100) == 0)
+        // {
+        //     printf("%d\n", i / (__UINT32_MAX__ / 100));
+        // }
+    }
+    end = clock();
+    double time1 = (double)(end - start) * 1000 / CLOCKS_PER_SEC;
+    printf("time1=%.20f\n", time1);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = 1; i <= n; i++)
+    {
+        unsigned r = my_isqrt_op(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
         max_err = fmax(max_err, e);
         max_rel_err = fmax(max_rel_err, r_e);
         err += e;
         rel_err += r_e;
     }
-    err = err / num_of_sample;
-    rel_err = rel_err / num_of_sample;
+    err = err / n;
+    rel_err = rel_err / n;
     end = clock();
-    printf("time1=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC);
-    printf("err1=%.20f\n", err);
-    printf("rel_err1=%.20f\n", rel_err);
-    printf("max_err1=%.20f\n", max_err);
-    printf("max_rel_err1=%.20f\n", max_rel_err);
+    printf("time2=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err2=%.20f\n", err);
+    printf("rel_err2=%.20f\n", rel_err);
+    printf("max_err2=%.20f\n", max_err);
+    printf("max_rel_err2=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / n);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = 1; i <= n; i++)
+    {
+        unsigned r = isqrt2(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+    }
+    err = err / n;
+    rel_err = rel_err / n;
+    end = clock();
+    printf("time3=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err3=%.20f\n", err);
+    printf("rel_err3=%.20f\n", rel_err);
+    printf("max_err3=%.20f\n", max_err);
+    printf("max_rel_err3=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / n);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = 1; i <= n; i++)
+    {
+        unsigned r = isqrt3(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+    }
+    err = err / n;
+    rel_err = rel_err / n;
+    end = clock();
+    printf("time4=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err4=%.20f\n", err);
+    printf("rel_err4=%.20f\n", rel_err);
+    printf("max_err4=%.20f\n", max_err);
+    printf("max_rel_err4=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / n);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = 1; i <= n; i++)
+    {
+        unsigned r = isqrt4(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+    }
+    err = err / n;
+    rel_err = rel_err / n;
+    end = clock();
+    printf("time5=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err5=%.20f\n", err);
+    printf("rel_err5=%.20f\n", rel_err);
+    printf("max_err5=%.20f\n", max_err);
+    printf("max_rel_err5=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / n);
+}
+
+void test_op_g()
+{
+    clock_t start, end;
+    double err = 0;
+    double max_err = 0;
+    double rel_err = 0;
+    double max_rel_err = 0;
+    unsigned n = __UINT32_MAX__ - ((1 << 8) + 1) * ((1 << 8) + 1);
+    start = clock();
+    for (unsigned i = ((1 << 8) + 1) * ((1 << 8) + 1); i != 0; i++)
+    {
+        unsigned r = sqrt(i);
+        // if (i % (__UINT32_MAX__ / 100) == 0)
+        // {
+        //     printf("%d\n", i / (__UINT32_MAX__ / 100));
+        // }
+    }
+    end = clock();
+    double time1 = (double)(end - start) * 1000 / CLOCKS_PER_SEC;
+    printf("time1=%.20f\n", time1);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = ((1 << 8) + 1) * ((1 << 8) + 1); i != 0; i++)
+    {
+        unsigned r = my_isqrt_op(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+    }
+    err = err / n;
+    rel_err = rel_err / n;
+    end = clock();
+    printf("time2=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err2=%.20f\n", err);
+    printf("rel_err2=%.20f\n", rel_err);
+    printf("max_err2=%.20f\n", max_err);
+    printf("max_rel_err2=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / n);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = ((1 << 8) + 1) * ((1 << 8) + 1); i != 0; i++)
+    {
+        unsigned r = isqrt2(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+    }
+    err = err / n;
+    rel_err = rel_err / n;
+    end = clock();
+    printf("time3=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err3=%.20f\n", err);
+    printf("rel_err3=%.20f\n", rel_err);
+    printf("max_err3=%.20f\n", max_err);
+    printf("max_rel_err3=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / n);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = ((1 << 8) + 1) * ((1 << 8) + 1); i != 0; i++)
+    {
+        unsigned r = isqrt3(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+    }
+    err = err / n;
+    rel_err = rel_err / n;
+    end = clock();
+    printf("time4=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err4=%.20f\n", err);
+    printf("rel_err4=%.20f\n", rel_err);
+    printf("max_err4=%.20f\n", max_err);
+    printf("max_rel_err4=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / n);
+
+    n_loop = 0;
+    err = 0;
+    max_err = 0;
+    rel_err = 0;
+    max_rel_err = 0;
+    start = clock();
+    for (unsigned i = ((1 << 8) + 1) * ((1 << 8) + 1); i != 0; i++)
+    {
+        unsigned r = isqrt4(i);
+        double t = sqrt(i);
+        double e = fabs(r - t);
+        double r_e = e / t;
+        max_err = fmax(max_err, e);
+        max_rel_err = fmax(max_rel_err, r_e);
+        err += e;
+        rel_err += r_e;
+    }
+    err = err / n;
+    rel_err = rel_err / n;
+    end = clock();
+    printf("time5=%.20f\n", (double)(end - start) * 1000 / CLOCKS_PER_SEC - time1);
+    printf("err5=%.20f\n", err);
+    printf("rel_err5=%.20f\n", rel_err);
+    printf("max_err5=%.20f\n", max_err);
+    printf("max_rel_err5=%.20f\n", max_rel_err);
+    printf("n_loop=%.20f\n", n_loop / n);
+}
+
+int main()
+{
+    // test_unsigned();
+    test_op_l();
+    test_op_g();
 }
